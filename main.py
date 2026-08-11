8e9e58573e5bf546762cf452562eb71d14c30490
snowdingo <snowdingo.tech@gmail.com>
2026-08-11 12:11:46 +0900

    built a timezone command that fetches time around the world

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import typer
 import currency
+import datetime
+import pytz
 
 app = typer.Typer()
 
@@ -17,6 +19,30 @@ def calcFX(original: str, convertcurrency: str):
     """
     exchangerate = currency.fetchCurrentExchangeRate(original,convertcurrency)
     print(f"The exchange rate of {original} to {convertcurrency} is {str(exchangerate)}")
+
+@app.command("timezone")
+def calcTimezone():
+    date = datetime.datetime.now()
+    print(f"now: " + date.strftime("%b") + " " + date.strftime("%d") + " " + date.strftime("%H") + ":" + date.strftime("%S"))
+    datenow = datetime.datetime.now(pytz.utc)
+    # Time zones
+    tz_ny = pytz.timezone('America/New_York')
+    tz_london = pytz.timezone('Europe/London')
+    tz_japan = pytz.timezone('Asia/Tokyo')
+    tz_china = pytz.timezone('Asia/Shanghai')
+    # convert to each local timezones
+    now_ny = datenow.astimezone(tz_ny)
+    now_lo = datenow.astimezone(tz_london)
+    now_jp = datenow.astimezone(tz_japan)
+    now_bj = datenow.astimezone(tz_china)
+    fmt = "%b %d %H:%M"
+    print("\n=AROUND THE WORLD=")
+    print("UTC: " + datenow.strftime(fmt))
+    print("Tokyo Japan: " + now_jp.strftime(fmt))
+    print("London UK: " + now_lo.strftime(fmt))
+    print("New York USA: " + now_ny.strftime(fmt))
+    print("Bejing China: " +now_bj.strftime(fmt))
+    return
 
 if __name__ == "__main__":
     app()
